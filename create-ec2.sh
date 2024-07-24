39e729fed839f4f0c150276ee5fce4cab9d1a41c
joindevops <info@joindevops.com>
2024-07-24 08:09:46 +0530

    functions

--- create-ec2.sh
+++ create-ec2.sh
@@ -16,7 +16,7 @@ for name in ${instances[@]}; do
 
     aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
 
-    if [ $name == "web "]
+    if [ $name == "web" ]
     then
         aws ec2 wait instance-running --instance-ids $instance_id
         public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
