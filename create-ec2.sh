ea1a6df9352ef69e5e697b3e8eb6e791a61d6d0a
joindevops <info@joindevops.com>
2024-07-24 07:38:49 +0530

    functions

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,5 +4,11 @@ instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 
 for name in ${instances[@]}; do
-    echo "Creating instance for: $name"
+    if [ $name == "shipping" ] || [ $name == "mysql" ]
+    then
+        instance_type="t3.medium"
+    else
+        instance_type="t3.micro"
+    fi
+    echo "Creating instance for: $name with instance type: $instance_type"
 done
