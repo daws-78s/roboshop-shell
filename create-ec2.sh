c76cd29a40d79720304b43f9dc83082cbf406779
joindevops <info@joindevops.com>
2024-07-24 08:11:50 +0530

    functions

--- create-ec2.sh
+++ create-ec2.sh
@@ -13,6 +13,7 @@ for name in ${instances[@]}; do
     fi
     echo "Creating instance for: $name with instance type: $instance_type"
     instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type t3.micro --security-group-ids sg-0fea5e49e962e81c9 --subnet-id subnet-09863c54177764565 --query 'Instances[0].InstanceId' --output text)
+    echo "Instance created for: $name"
 
     aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
 
@@ -26,7 +27,8 @@ for name in ${instances[@]}; do
         ip_to_use=$private_ip
     fi
 
-    aws route53 change-resource-record-sets -hosted-zone-id $zoneid --change-batch '
+    echo "creating R53 record for $name"
+    aws route53 change-resource-record-sets -hosted-zone-id $hosted_zone_id --change-batch '
     {
         "Comment": "Creating a record set for '$name'"
         ,"Changes": [{
