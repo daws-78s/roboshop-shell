cd9c01cd1d024491ae168d152641ba0f5d25cbab
joindevops <info@joindevops.com>
2024-07-26 08:13:58 +0530

    functions

---
+++ shipping.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+
+dnf install maven -y &>> $LOGFILE
+VALIDATE $? "Installing Maven"
+
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "Adding roboshop user"
+else
+    echo -e "roboshop user already exist...$Y SKIPPING $N"
+fi
+
+rm -rf /app &>> $LOGFILE
+VALIDATE $? "clean up existing directory"
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+VALIDATE $? "Downloading shipping application"
+
+cd /app  &>> $LOGFILE
+VALIDATE $? "Moving to app directory"
+
+unzip /tmp/shipping.zip &>> $LOGFILE
+VALIDATE $? "Extracting shipping application"
+
+mvn clean package &>> $LOGFILE
+VALIDATE $? "Packaging shipping"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+VALIDATE $? "Renaming the artifact"
+
+cp /home/ec2-user/roboshop-shell/shipping.service etc/systemd/system/shipping.service &>> $LOGFILE
+VALIDATE $? "Copying service file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable shipping  &>> $LOGFILE
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping &>> $LOGFILE
+VALIDATE $? "Starting shipping"
+
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing MySQL"
